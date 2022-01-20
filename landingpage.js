53a593bfd04d9f0960408c3e80b491e1227f7a1f
Hyemi Jo <92564768+JoHyemi@users.noreply.github.com>
2022-01-20 21:45:18 +0900

    반응형 추가

--- landingpage.js
+++ landingpage.js
@@ -1,7 +1,7 @@
-const copyUrl = document.querySelector('li .btn-copy');
-const copyUrl2 = document.querySelector('p+.btn-copy');
+const copyUrl = document.querySelectorAll('.btn-copy');
 
-copyUrl.addEventListener('click', () => {
+for(let i = 0; i < 3; i++){
+  copyUrl[i].addEventListener('click', () => {
     let url = '';
     let urlText = document.createElement('textarea');
     document.body.appendChild(urlText);
@@ -12,17 +12,7 @@ copyUrl.addEventListener('click', () => {
     document.body.removeChild(urlText);
     alert('URL이 복사되었습니다:)')
 })
-copyUrl2.addEventListener('click', () => {
-    let url = '';
-    let urlText = document.createElement('textarea');
-    document.body.appendChild(urlText);
-    url = window.document.location.href;
-    urlText.value = url;
-    urlText.select();
-    document.execCommand('copy');
-    document.body.removeChild(urlText);
-    alert('URL이 복사되었습니다:)')
-})
+}
 
 const subscribeBtn = document.querySelector('.btn-subscribe');
 const modal = document.querySelector('.subscribe-modal');
@@ -36,3 +26,43 @@ subscribeBtn.addEventListener('click', () => {
 modalBtn.addEventListener('click', () => {
     modal.style.display = 'none';
 });
+
+
+const upBtn = document.querySelector('.btn-up');
+
+upBtn.addEventListener('click', () => {
+    scrollTo({ top: 0, behavior: 'smooth' });
+});
+
+const menuHome = document.querySelectorAll('.menu-home');
+const menuAbout = document.querySelectorAll('.menu-about');
+const menuSubscribe = document.querySelectorAll('.menu-subscribe');
+
+for(let i = 0; i < 2;i++) {
+  menuHome[i].addEventListener('click', () => {
+    document.querySelector('.header').scrollIntoView();
+  dropMenu.style.display = 'none'
+})
+}
+for(let i = 0; i < 2;i++) {
+menuAbout[i].addEventListener('click', () => {
+    document.querySelector('main').scrollIntoView();
+    dropMenu.style.display = 'none'
+})
+}
+for(let i = 0; i < 2;i++) {
+menuSubscribe[i].addEventListener('click', () => {
+    document.querySelector('.sec-subscribe').scrollIntoView();
+    dropMenu.style.display = 'none'
+})
+}
+const openMenu = document.querySelector('.open-menu');
+const closeMenu = document.querySelector('.menu-close');
+const dropMenu = document.querySelector('.drop-menu');
+
+openMenu.addEventListener('click', () => {
+  dropMenu.style.display = 'block'
+})
+closeMenu.addEventListener('click', ()=> {
+  dropMenu.style.display = 'none'
+})
